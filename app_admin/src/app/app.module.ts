d0a41ade28710c8e88f28a2b779c994bdd6cc3b2
drake perry <drakep94@live.com>
2022-08-12 17:49:18 -0500

    new additions

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { AddTripComponent } from './add-trip/add-trip.component';
+import { EditTripComponent } from './edit-trip/edit-trip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TripListingComponent
+    TripListingComponent,
+    TripCardComponent,
+    AddTripComponent,
+    EditTripComponent
   ],
   imports: [
     BrowserModule
